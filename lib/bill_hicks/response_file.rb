4da3fe7e2d80cb68ac2f5169ec4a5c8d31f8dcda
Justin Wilson <justin@jwils.co>
2016-12-22 10:10:52 -0500

    - Predicate methods for proprietary file types - Handle a malformed ASN file

--- lib/bill_hicks/response_file.rb
+++ lib/bill_hicks/response_file.rb
@@ -1,14 +1,8 @@
 module BillHicks
   class ResponseFile < Base
 
-    attr_reader :content
     attr_reader :credentials
     attr_reader :filename
-
-    FILE_TYPES = {
-      "ACK" => "Purchase Order Acknowledgement",
-      "ASN" => "Advance Shipping Notice"
-    }
 
     # @option options [String] :username *required*
     # @option options [String] :password *required*
@@ -32,6 +26,17 @@ module BillHicks
       end
     end
 
+    # Is the file a '855 Purchase Order Acknowledgement'?
+    def ack?
+      content.start_with?("ACK")
+    end
+
+    # Is the file a '856 Advance Shipping Notice'?
+    def asn?
+      content.start_with?("ASN")
+    end
+
+    # Use '#gettextfile' to read file contents as a string
     def content
       return @content if @content
       connect(@credentials) do |ftp|
@@ -40,10 +45,30 @@ module BillHicks
       end
     end
 
+    # Convert to easily readable key-value pairs
     def to_json
-      CSV.parse(content, headers: true, col_sep: "|").
-        map { |x| x.to_h }.
-        group_by { |x| x["PO Number"] }
+      if corrupt_asn?
+        CSV.parse(content.gsub("Price|", ""), headers: true, col_sep: "|").
+          map { |x| x.to_h }.
+          group_by { |x| x["PO Number"] }
+      else
+        CSV.parse(content, headers: true, col_sep: "|").
+          map { |x| x.to_h }.
+          group_by { |x| x["PO Number"] }
+      end
+    end
+
+    private
+
+    def corrupt_asn?
+      return false if ack?
+      lines = content.lines.map(&:chomp)
+      if lines[0].split("|").length != lines[1].split("|").length
+        puts "Notice: ASN file is malformed! (#{filename})"
+        true
+      else
+        false
+      end
     end
 
   end
--- spec/bill_hicks/response_file_spec.rb
+++ spec/bill_hicks/response_file_spec.rb
@@ -17,6 +17,46 @@ describe BillHicks::ResponseFile do
     it { expect(all.length).to eq(2) }
   end
 
+  describe '#ack?' do
+    let(:response_file) { BillHicks::ResponseFile.new(username: "user", password: "pass", filename: '20161117-0001.txt') }
+
+    context "is an ACK file" do
+      before do
+        allow(response_file).to receive(:content) { sample_ack_file }
+      end
+
+      it { expect(response_file.ack?).to be(true) }
+    end
+
+    context "is NOT an ACK file" do
+      before do
+        allow(response_file).to receive(:content) { sample_asn_file }
+      end
+
+      it { expect(response_file.ack?).to be(false) }
+    end
+  end
+
+  describe '#asn?' do
+    let(:response_file) { BillHicks::ResponseFile.new(username: "user", password: "pass", filename: '20161117-0001.txt') }
+
+    context "is an ASN file" do
+      before do
+        allow(response_file).to receive(:content) { sample_asn_file }
+      end
+
+      it { expect(response_file.asn?).to be(true) }
+    end
+
+    context "is NOT an ASN file" do
+      before do
+        allow(response_file).to receive(:content) { sample_ack_file }
+      end
+
+      it { expect(response_file.asn?).to be(false) }
+    end
+  end
+
   describe '#content' do
     let(:filename) { '20161117-0001.txt' }
     let(:response_file) { BillHicks::ResponseFile.new(credentials.merge(filename: filename)) }
@@ -33,19 +73,38 @@ describe BillHicks::ResponseFile do
   end
 
   describe '#to_json' do
-    let(:filename) { '20161117-0001.txt' }
-    let(:response_file) { BillHicks::ResponseFile.new(credentials.merge(filename: filename)) }
+    context 'all is fine' do
+      let(:filename) { '20161117-0001.txt' }
+      let(:response_file) { BillHicks::ResponseFile.new(credentials.merge(filename: filename)) }
 
-    before do
-      allow(response_file).to receive(:content) { sample_ack_file }
-      @json = response_file.to_json
+      before do
+        allow(response_file).to receive(:content) { sample_ack_file }
+        @json = response_file.to_json
+      end
+
+      it { expect(@json["999999910"][0]).to have_key("PO Date") }
+      it { expect(@json["999999910"][0]).to have_key("PO Number") }
+      it { expect(@json["999999910"][0]).to have_key("Quantity Ordered") }
+      it { expect(@json["999999910"][0]).to have_key("Quantity Committed") }
+      it { expect(@json["999999910"][0]).to have_key("UPC Number") }
+      it { expect(@json["999999910"][0]).to have_key("BHC Order Number") }
     end
 
-    it { expect(@json["999999910"][0]).to have_key("PO Date") }
-    it { expect(@json["999999910"][0]).to have_key("PO Number") }
-    it { expect(@json["999999910"][0]).to have_key("Quantity Ordered") }
-    it { expect(@json["999999910"][0]).to have_key("Quantity Committed") }
-    it { expect(@json["999999910"][0]).to have_key("UPC Number") }
+    context 'bad ASN file' do
+      let(:filename) { '20161117-0001.txt' }
+      let(:response_file) { BillHicks::ResponseFile.new(credentials.merge(filename: filename)) }
+
+      before do
+        allow(response_file).to receive(:content) { sample_bad_asn_file }
+        @json = response_file.to_json
+      end
+
+      it { expect(@json["999999910"][0]).to have_key("PO Date") }
+      it { expect(@json["999999910"][0]).to have_key("UPC Number") }
+      it { expect(@json["999999910"][0]).to have_key("Tracking Number") }
+      it { expect(@json["999999910"][0]).to have_key("Carrier") }
+      it { expect(@json["999999910"][0]).to have_key("BHC Order Number") }
+    end
   end
 
 end
