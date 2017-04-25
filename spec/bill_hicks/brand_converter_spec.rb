37bb65758068868c3c7ed865fbf63a0875c8d8e6
Justin Wilson <justin@jwils.co>
2017-04-25 11:04:28 -0400

    Better formatting and mapping for brands

--- spec/bill_hicks/brand_converter_spec.rb
+++ spec/bill_hicks/brand_converter_spec.rb
@@ -4,15 +4,15 @@ describe BillHicks::BrandConverter do
 
   describe '.convert' do
     it 'finds the corresponding brand for 2A' do
-      expect(BillHicks::BrandConverter.convert('2A')).to eq('2A ARMAMENT')
+      expect(BillHicks::BrandConverter.convert('2A')).to eq('2A Armament')
     end
 
     it 'finds the corresponding brand for GLOCK' do
-      expect(BillHicks::BrandConverter.convert('GLOCK')).to eq('GLOCK')
+      expect(BillHicks::BrandConverter.convert('GLOCK')).to eq('Glock')
     end
 
     it 'finds the corresponding brand for OAI' do
-      expect(BillHicks::BrandConverter.convert('oai')).to eq('OLYMPIC ARMS INC.')
+      expect(BillHicks::BrandConverter.convert('oai')).to eq('Olympic Arms')
     end
   end
 
